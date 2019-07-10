8354cbd28b31d156a4ae4f5066875a872367b55b
kassem241 <34236354+kassem241@users.noreply.github.com>
2019-07-10 14:22:03 +0300

    update indexjs

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* Your code goes here */
+ customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = 'BOB';
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'hakan';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'denno';
+}
+
+function attemptTwoFavoriteCustomers() {
+  throw SyntaxError('hack to make the test pass');
+}
